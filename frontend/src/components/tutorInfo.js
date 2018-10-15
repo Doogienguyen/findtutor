d5993833a08c3ed5490efd4cc4825b47661fa97e
JohntPark <JohnTaeHeePark@gmail.com>
2018-10-15 15:10:21 -0700

    updated tutorInfo

--- frontend/src/components/tutorInfo.js
+++ frontend/src/components/tutorInfo.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import { getTeachers, changePath} from '../actions/index';
 
 
 class TutorInfo extends Component {
@@ -15,33 +16,49 @@ render() {
         this.props.teacher && 
         <div>
             <div>
+                            <div className="header" style={{backgroundColor: "#f0d4c2"}}>
+                    <a href="#default" className="logo">Tutor To-Go</a>
+                    <div className="header-right">
+                        <a className="active" href="#home">Home</a>
+                        <a href="#contact">Contact</a>
+                        <a onClick={()=> this.props.changePath("TUTORS")}>Tutors</a>
+                        <a onClick = {() => this.props.changePath("SIGNUP")}>Sign Up</a>
+                    </div>
+                </div>
+            </div>
+            <div>
                 <ul>
                     <div className = "backgroundImageProfile">
-                        <li><img style ={{height: 160, width: 160, borderRadius: 35 }} src={this.props.teacher.imgUrl} alt="Teacher Photo"/></li>
+                        {/* <li><img style ={{height: 100, width: 100, borderRadius: 35 }} src={this.props.teacher.imgUrl} alt="Teacher Photo"/></li> */}
                     </div>
+                    <div className = "mapBorder" style={{backgroundColor: "#cbd3dd"}}>
+                    <div className='mapContainer'>
                     <div className = "mainContent">
                     <div className="sidebarInfo">
                         <div className = "textStyleProfile">
+                        <li><img style ={{marginLeft: 30, paddingTop: 10, height: 120, width: 120, borderRadius: 35 }} src={this.props.teacher.imgUrl} alt="Teacher Photo"/></li>
                         <li><h4><img className="sideBarIcon" src={"https://embodiedfacilitator.com/wp-content/uploads/2018/05/human-icon-png-1901.png"} alt="name icon"/>{this.props.teacher.name} </h4></li>
                         <li><h4><img className="sideBarIcon" src={"http://clipartmag.com/images/transparent-email-icon-38.png"} alt="email icon"/>{this.props.teacher.email} </h4></li>
                         <li><h4><img className="sideBarIcon" src={"https://cdn.onlinewebfonts.com/svg/img_198352.png"} alt="Phone icon"/>{this.props.teacher.phone} </h4></li>
                         <li><h4><img className="sideBarIcon" src={"https://www.freeiconspng.com/uploads/google-location-icon-16.png"} alt="location icon"/>{this.props.teacher.location} </h4></li>
                         </div>
                     </div>
-                    <div style = {{flex: 4, fontSize: 5}}>
+                    <div style = {{flex: 7, fontSize: 5}}>
                         <div className = "firstSection">
-                        <li><h5> Specialization: {this.props.teacher.specialization} </h5></li>
-                        <li><h5> Education Level: {this.props.teacher.educationLevel} </h5></li>
-                        <li><h5> Subjects Offered: {this.props.teacher.subjectsOffered} </h5></li>
+                        <li><h4><strong> Specialization:</strong> <small>{this.props.teacher.specialization}</small> </h4></li>
+                        <li><h4><strong> Education Level:</strong> <small>{this.props.teacher.educationLevel} </small></h4></li>
+                        <li><h4><strong> Subjects Offered:</strong> <small>{this.props.teacher.subjectsOffered} </small></h4></li>
                         </div>
                         <div className = "secondSection">
-                        <li><h5> Average Hourly Rate: {this.props.teacher.avgHourlyRate} </h5></li>
-                        <li><h5> Availability: {this.props.teacher.availability} </h5></li>
+                        <li><h4><strong> Average Hourly Rate:</strong> <small>{this.props.teacher.avgHourlyRate} </small></h4></li>
+                        <li><h4><strong> Availability:</strong> <small>{this.props.teacher.availability} </small></h4></li>
                         </div>
-                        <div className = "thirdSection">
-                        <li><h5> Biography: {this.props.teacher.bio} </h5></li>
-                        <li><h5> Education History: {this.props.teacher.educationHistory} </h5></li>
+                        <div className = "thirdSection" style ={{height: '100%'}}>
+                        <li><h4><strong> Biography:</strong><small> {this.props.teacher.bio} </small></h4></li>
+                        <li><h4><strong> Education History:</strong> <small>{this.props.teacher.educationHistory} </small></h4></li>
                         </div>
+                    </div>
+                    </div>
                     </div>
                     </div>
                 </ul> 
@@ -69,5 +86,11 @@ const mapStateToProps = state => ({
     teacherID: state.ID,
     teacher: state.teacher
   })
+  const mapDispatchToProps = dispatch => ({
+    getTeachers: () => dispatch(getTeachers()),
+    changePath: (path) => dispatch(changePath(path)),
+  
+  })
+  
 
-export default connect(mapStateToProps)(TutorInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(TutorInfo);
