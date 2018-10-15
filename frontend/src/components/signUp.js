72ef726b319f5a1fcea8bd4db9ef87037241dc8c
Doogie Nguyen <doogie@Doogies-MBP.attlocal.net>
2018-10-15 15:48:04 -0700

    deleted button

--- frontend/src/components/signUp.js
+++ frontend/src/components/signUp.js
@@ -46,14 +46,7 @@ class SignUp extends Component {
                                     <input type="submit" name="" value="Login" /><br />
                                 </div>
                                 <div class="col-md-9 register-right">
-                                    <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Tutor</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Student</a>
-                                        </li>
-                                    </ul>
+      
                                     <div class="tab-content" id="myTabContent">
                                         <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                                             <h3 class="register-heading">Apply as a tutor</h3>
@@ -117,32 +110,7 @@ class SignUp extends Component {
                                                     <input type="submit" class="btnRegister" value="Sign Up" />
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="tab-pane fade show" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                            <h3 class="register-heading">Apply as a student</h3>
-                                            <div class="row register-form">
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <input type="text" class="form-control" placeholder="First Name" value={this.state.FirstName} onChange={this.onFirstNameChange} />
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <input type="text" class="form-control" placeholder="Last Name" value={this.state.lastName} onChange={this.onLastNameChange} />
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <input type="email" class="form-control" placeholder="Email" value={this.state.email} onChange={this.onEmail} />
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <input type="password" class="form-control" placeholder="Password" value={this.state.password} onChange={this.onPassword} />
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <input type="password" class="form-control" placeholder="Confirm Password" value={this.state.confirmPassword} onChange={this.onConfirmPasswordChange} />
-                                                    </div>
-                                                    <input type="submit" class="btnRegister" value="Sign Up" />
-                                                </div>
-                                            </div>
-                                        </div>
+                                        </div>                                 
                                     </div>
                                 </div>
                             </div>
